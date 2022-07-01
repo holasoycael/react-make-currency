d685f1a46ab4e16c909fb37300905ec6f5916d42
Natanael Lourenço <holasoycael@gmail.com>
2022-07-01 14:09:41 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+export function onValueChange(e: React.FormEvent<HTMLInputElement>) {
+  const targetValue = e.currentTarget.value
+
+  let currenctValue = targetValue
+  currenctValue = currenctValue.replace(/\D/g, '')
+  currenctValue = currenctValue.replace(/(\d)(\d{2})$/, '$1,$2')
+  currenctValue = currenctValue.replace(/(?=(\d{3})+(\D))\B/g, '.')
+
+  const [thousand, decimal] = currenctValue.split(',')
+  const parseThousand = thousand.replace(/\D/g, '')
+
+  const parseFloatValue = decimal
+    ? `${parseThousand}.${decimal}`
+    : `0.${parseThousand.length === 1 ? `0${parseThousand}` : parseThousand}`
+
+  const floatValue = parseFloat(parseFloatValue)
+
+  return { floatValue, stringValue: toString(floatValue) }
+}
+
+export function isValid(e: React.FormEvent<HTMLInputElement>, max: number) {
+  const targetValue = e.currentTarget.value
+  return targetValue.replace(/\D/g, '').length < max
+}
+
+export function toString(value: number) {
+  const defaultPrice = value.toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL'
+  })
+  const formatValue = defaultPrice.slice(3)
+
+  return value ? formatValue : ''
+}
+
+export type TOnValueChange = ReturnType<typeof onValueChange>
